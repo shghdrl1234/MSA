25fe2e1dc94726ecae1a99bb343f1a4a494a620c
DESKTOP-8ROT1QV\교육생11 <shghdrl1234@gmail.com>
2023-04-25 21:40:40 +0900

    mave to gradle

--- user-service/src/main/java/com/example/userservice/UserServiceApplication.java
+++ user-service/src/main/java/com/example/userservice/UserServiceApplication.java
@@ -1,11 +1,12 @@
 package com.example.userservice;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-@EnableEurekaClient //
+@EnableDiscoveryClient
 public class UserServiceApplication {
 
 	public static void main(String[] args) {
