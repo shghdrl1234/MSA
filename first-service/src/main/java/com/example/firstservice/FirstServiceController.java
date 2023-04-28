a45e3ac6bbd4e9ab0de00a8d0c09d7aa59a02bf5
DESKTOP-8ROT1QV\교육생11 <shghdrl1234@gmail.com>
2023-04-28 21:39:31 +0900

    load balancer

--- first-service/src/main/java/com/example/firstservice/FirstServiceController.java
+++ first-service/src/main/java/com/example/firstservice/FirstServiceController.java
@@ -1,6 +1,10 @@
 package com.example.firstservice;
 
+import com.netflix.discovery.converters.Auto;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,6 +14,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/first-service")
 @Slf4j
 public class FirstServiceController {
+
+    Environment env;
+    
+    @Autowired // 생성자 통해서 주입
+    public FirstServiceController(Environment env) {
+        this.env = env;
+    }
+
     @GetMapping("/welcome")
     public String welcome() {
         return "Welcome to the First service.";
@@ -22,7 +34,10 @@ public class FirstServiceController {
     }
 
     @GetMapping("/check")
-    public String check() {
-        return "1번 서비스 확인.";
+    public String check(HttpServletRequest request) {
+        log.info("서버 포트 = {}", request.getServerPort());
+
+        return String.format("1번 서비스 확인.", env .getProperty("local.server port"));
     }
+
 }
